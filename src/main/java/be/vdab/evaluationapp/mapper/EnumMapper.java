16e4c2173eeb34f4ff5bec0deede9a17947002b3
Aaron <aaronvanstappen@gmail.com>
2017-11-20 16:00:19 +0100

    - Enums and EnumMapper finished

--- src/main/java/be/vdab/evaluationapp/mapper/EnumMapper.java
+++ src/main/java/be/vdab/evaluationapp/mapper/EnumMapper.java
@@ -1,4 +1,34 @@
 package be.vdab.evaluationapp.mapper;
 
+import be.vdab.evaluationapp.model.AnswerType;
+import be.vdab.evaluationapp.model.CourseTopic;
+import be.vdab.evaluationapp.model.QuestionType;
+
 public class EnumMapper {
+    public static AnswerType mapToAnswerType(int answer) {
+        for(AnswerType a: AnswerType.values()) {
+            if(a.ordinal() == answer) {
+                return a;
+            }
+        }
+        throw new RuntimeException("AnswerType not found");
+    }
+
+    public static QuestionType mapToQuestionType(int question) {
+        for(QuestionType q : QuestionType.values()) {
+            if(q.ordinal() == question) {
+                return q;
+            }
+        }
+        throw new RuntimeException("QuestionType not found");
+    }
+
+    public static CourseTopic mapToCourseTopic(int courseTopic) {
+        for(CourseTopic c : CourseTopic.values()) {
+            if(c.ordinal() == courseTopic) {
+                return c;
+            }
+        }
+        throw new RuntimeException("CourseTopic not found");
+    }
 }
--- src/main/java/be/vdab/evaluationapp/model/CourseTopic.java
+++ src/main/java/be/vdab/evaluationapp/model/CourseTopic.java
@@ -1,11 +1,11 @@
 package be.vdab.evaluationapp.model;
 
-public enum Topic {
+public enum CourseTopic {
     ADOBE("Adobe"), BUSINESS_INTELLIGENCE("Business Intelligence"), GENERAL_PROGRAMMING("General Programming"), PROJECT_MANAGEMENT("ITIL and Project Management"), JAVA("Java"), SAP("SAP"), METHODOLOGY("Methodology"), MICROSOFT_CRM("Microsoft CRM"), MICROSOFT("Microsoft"), ORACLE("Oracle"), TESTING("Testing"), UNIXLINUX("Unix/Linux"), WEB_DESIGN("Web Design"), XML("XML"), MOBILE("Mobile Apps");
 
     private String string;
 
-    Topic(String string) {
+    CourseTopic(String string) {
         this.string = string;
     }
 
