ba5f14065b2bfb24b9ba8d46c99a7f0d0bf0751b
Aaron <aaronvanstappen@gmail.com>
2017-11-20 11:26:44 +0100

    - project initialized

---
+++ src/main/java/be/vdab/evaluationapp/EvaluationappApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.evaluationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationappApplication.class, args);
+	}
+}
