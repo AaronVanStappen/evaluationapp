902c4b4937898b539e277a3915223ed8f7b46627
Valerie <valerie.meers@gmail.com>
2017-11-21 12:33:10 +0100

    add repositoryImpl

---
+++ src/main/java/be/vdab/evaluationapp/repository/EvaluationRepositoryImpl.java
@@ -0,0 +1,57 @@
+package be.vdab.evaluationapp.repository;
+
+import be.vdab.evaluationapp.model.Answer;
+import be.vdab.evaluationapp.model.Instructor;
+import be.vdab.evaluationapp.model.Question;
+import be.vdab.evaluationapp.model.Trainee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class EvaluationRepositoryImpl implements EvaluationRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    EvaluationRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    @Transactional
+    public void addEvaluation(Trainee trainee, Instructor instructor, Question question, Answer answer){
+        addTrainee(trainee);
+        addInstructor(instructor);
+        addQuestion(question);
+        addAnswer(answer);
+    }
+
+    private int addTrainee(Trainee trainee){
+
+        String sql = "INSERT INTO trainee(firstname, name, email, date, phone, company, jobrole) VALUES (?,?,?,?,?,?,?)";
+
+        return jdbcTemplate.update(sql,trainee.getFirstname(), trainee.getName(), trainee.getEmail(), trainee.getDate(),
+                trainee.getPhonenumber(), trainee.getCompanyName(), trainee.getJobRole());
+    }
+
+    private int addInstructor(Instructor instructor){
+        String sql = "INSERT INTO instructor(firstname, name) VALUES (?,?)";
+
+        return jdbcTemplate.update(sql, instructor.getFirstname(), instructor.getName());
+    }
+
+    private int addQuestion(Question question){
+        String sql = "INSERT INTO question(question, questionTypeId, answerTypeId) VALUES (?,?,?)";
+
+        return jdbcTemplate.update(sql, question.getQuestion(), question.getQuestionTypeId(), question.getAnswerTypeId());
+    }
+
+    private int addAnswer(Answer answer){
+        String sql = "INSERT INTO answer (answer, traineeid, questiontypeid, instructorcourseid, answertypeid) VALUES (?,?,?,?,?)";
+
+        return jdbcTemplate.update(sql, answer.getAnswer(), answer.getTraineeId(), answer.getQuestionTypeId(),
+                answer.getInstructorCourseId(), answer.getAnswerTypeId());
+    }
+}
