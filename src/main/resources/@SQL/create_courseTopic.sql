dd131b364814d6cbf9698ed4a8541352038cc8aa
Unknown <aaronvanstappen@gmail.com>
2017-11-21 11:29:45 +0100

    Revert "ccc"
    This reverts commit dc5f619afbe8582fab2b50e4b32b20d15934b3a5.

--- src/main/resources/@SQL/create_courseTopic.sql
+++
@@ -1,7 +0,0 @@
--- DROP TABLE `eval`.`coursetopic`;
-
-CREATE TABLE `eval`.`coursetopic` (
-  `id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- src/main/resources/@SQL/create_question.sql
+++
@@ -1,8 +0,0 @@
--- DROP TABLE `eval`.`question`;
-
-CREATE TABLE `eval`.`question` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `question` VARCHAR(120) NOT NULL,
-  `questionTypeId` INT NOT NULL,
-  `answerTypeId` INT NOT NULL,
-  PRIMARY KEY (`id`));
