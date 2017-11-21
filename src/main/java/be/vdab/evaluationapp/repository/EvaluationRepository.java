de48af2d0fe1d1dfc9b294c17c5dee61e02a9bc5
Valerie <valerie.meers@gmail.com>
2017-11-21 12:31:25 +0100

    add Repository + repositoryImpl

--- src/main/java/be/vdab/evaluationapp/repository/EvaluationRepository.java
+++ src/main/java/be/vdab/evaluationapp/repository/EvaluationRepository.java
@@ -1,7 +1,12 @@
 package be.vdab.evaluationapp.repository;
 
+import be.vdab.evaluationapp.model.Answer;
+import be.vdab.evaluationapp.model.Instructor;
+import be.vdab.evaluationapp.model.Question;
 import be.vdab.evaluationapp.model.Trainee;
 
 public interface EvaluationRepository {
-    Trainee trainee = new Trainee();
+
+    void addEvaluation (Trainee trainee, Instructor instructor, Question question, Answer answer);
+
 }
